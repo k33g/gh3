22f570e3dc400f56492676d36670e6a0afbf5be7
k33g_org <ph.charriere@gmail.com>
2012-07-25 22:41:58 +0200

    first version 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,314 @@
 gh3
 ===
 
-Client-side Javascript API wrapper for GitHub
+**gh3** is a client-side Javascript API v3 wrapper for GitHub
+
+##Dependencies
+
+- [jQuery](http://jquery.com/)
+- [Underscore](http://underscorejs.org/)
+
+##Bundled with gh3 :
+
+gh3 cames with 9 kinds of object's type :
+
+- `Gh3.User`
+- `Gh3.Repository`
+- `Gh3.Branch`
+- `Gh3.Dir` inherits of `Gh3.ItemContent`
+- `Gh3.File` inherits of `Gh3.ItemContent`
+- `Gh3.Commit`
+- `Gh3.Gists`
+- `Gh3.Gist`
+- `Gh3.GistComment`
+
+You can extend all Gh3 types, eg :
+
+	var myComment = Gh3.GistComment.extend({
+		//...
+	})
+
+See *§ "Extend User"* for more details.
+
+##GitHub User : Gh3.User
+
+###Declare User
+
+	var k33g = new Gh3.User("k33g");
+
+####Methods of Gh3.User
+
+- `fetch(callback, callbackErr)` : retrieve GitHub User's informations
+
+###Get User data
+
+	k33g.fetch(function (){
+		console.log("Blog : ", k33g.blog);
+		console.log("Name : ", k33g.name);
+		//etc. ...
+	});
+
+or :
+
+	k33g.fetch(function (user){
+		console.log("Blog : ", user.blog);
+		console.log("Name : ", user.name);
+		//etc. ...
+	});
+
+See [/samples/01-user.html](/samples/01-user.html)
+
+###Extend User
+
+I've cribbed the [Backbone](http://backbonejs.org/) object model :
+
+	var GitHubUser = Gh3.User.extend({//instance members
+		constructor : function(login) {
+			GitHubUser.__super__.constructor.call(this, login);
+			GitHubUser.allUsers.push(this);
+		}
+	},{ //Static members
+		allUsers : [],
+		each : function (callback) {
+			_.each(GitHubUser.allUsers, function (user) {
+				callback(user);
+			})
+		}
+	});
+
+	var k33g = new GitHubUser("k33g")
+	,	loicdescotte = new GitHubUser("loicdescotte")
+	,	mklabs = new GitHubUser("mklabs")
+	,	chamerling = new GitHubUser("chamerling");
+	
+	GitHubUser.each(function (user) {
+		user.fetch(function () {
+			console.log(JSON.stringify(user));
+		});
+	})
+
+See [/samples/02-user.html](/samples/02-user.html)
+
+##GitHub Repository : Gh3.Repository
+
+###Declare and Get Repository informations
+
+	var k33g = new Gh3.User("k33g"); // You need a user first
+
+	var k33gBlog = new Gh3.Repository("k33g.github.com", k33g);
+
+	k33gBlog.fetch(function () {
+		console.log(k33gBlog);
+	});
+
+####Methods of Gh3.Repository
+
+- `getBranches` : return array of `Gh3.Branch` instances
+- `fetch(callback, callbackErr)` : retrieve repository's informations
+- `fetchBranches(callback, callbackErr)` : retrieve repository's branches (you have to call `fetch()` before)
+- `getBranchByName(branch_name)` : return a `Gh3.Branch` instance found by name (you have to call `fetchBranches()` before)
+- `eachBranch(callback)` : execute `callback` for each branch of the repository (you have to call `fetchBranches()` before)
+
+###Get Branches of Repository
+
+After calling repository `fetch()` method :
+
+	k33gBlog.fetchBranches(function () {
+		console.log(k33gBlog.getBranches());
+	})
+
+`branches` is an array of `Gh3.Branch` instances populated when `fetchBranches()` is called.
+
+You can do that too :
+
+	k33gBlog.fetchBranches(function () {
+		console.log("Array of branches : ", k33gBlog.getBranches());
+		k33gBlog.eachBranch(function (branch) {
+			console.log(branch.name);
+		})
+
+		//and :
+		console.log("Master Branch : ", k33gBlog.getBranchByName("master"));
+
+	})
+
+See [/samples/03-repository.html](/samples/03-repository.html)
+
+##GitHub Branch : Gh3.Branch
+
+####Methods of Gh3.Branch
+
+- `fetchContents(callback, callbackErr)` : retrieve `Gh3.File` instances and/or `Gh3.Dir` instances of the branch
+- `getContents()` : return array of `Gh3.ItemContent` instances (so `Gh3.File` instances and/or `Gh3.Dir` instances)
+- `getFileByName(file_name)` : return a `Gh3.File` instance found by name (you have to call `fetchContents()` before)
+- `getDirByName(dir_name)` : return a `Gh3.Dir` instance found by name (you have to call `fetchContents()` before)
+- `eachContent(callback)` : execute `callback` for each content item of the branch (you have to call `fetchContents()` before)
+
+##Get contents of a branch
+
+You have to declare, get repository informations and fetch branches before calling `fetchContents(callback)` :
+
+	var k33g = new Gh3.User("k33g")
+	,	k33gBlog = new Gh3.Repository("k33g.github.com", k33g);
+
+	k33gBlog.fetch(function () {
+
+		k33gBlog.fetchBranches(function () {
+
+			var master = k33gBlog.getBranchByName("master");
+
+			master.fetchContents(function () {
+				master.eachContent(function (content) {
+					console.log(content.path, content.type);
+				});
+			});
+
+		})
+	});
+
+See [/samples/04-branch.html](/samples/04-branch.html)
+
+You obtain a list of files and directories. You can directly fetch raw content of a file or fetch contents of a directory.
+
+##GitHub File : Gh3.File
+
+####Methods of Gh3.File
+
+- `fetchContent(callback, callbackErr)` : retrieve (raw) content of the file
+- `fetchCommits(callback, callbackErr)` : retrieve commits of the file
+- `getRawContent()` : return raw content of the file (you have to call `fetchContent()` before)
+- `getCommits()` : return array of `Gh3.Commit` instances (you have to call `fetchCommits()` before)
+- `getLastCommit()` : return last `Gh3.Commit` instance
+- `getFirstCommit()` : return first `Gh3.Commit` instance
+- `eachCommit(callback)` : execute `callback` for each commit of the file (you have to call `fetchCommits()` before)
+
+###Get raw content of a file
+
+	master.fetchContents(function () {
+
+		var myfile = master.getFileByName("index.html");
+
+		/* this way is possible to :
+			var myfile = master.getContents()[8];
+		*/
+
+		myfile.fetchContent(function () {
+			console.log(myfile.getRawContent());
+		});
+		
+	});
+
+See [/samples/05-file.html](/samples/05-file.html)
+
+###Get commits of a file
+
+		myfile.fetchCommits(function () {
+			console.log(myfile.getCommits()); 
+
+			myfile.eachCommit(function (commit) {
+				console.log(commit.author.login, commit.message, commit.date);
+			});
+		});
+
+
+See [/samples/05-file.html](/samples/05-file.html)
+
+##GitHub Directory : Gh3.Dir
+
+It works much like a `Gh3.Branch`.
+
+####Methods of Gh3.Dir
+
+- `fetchContents(callback, callbackErr)` : retrieve `Gh3.File` instances and/or `Gh3.Dir` instances of the directory
+- `getContents()` : return array of `Gh3.ItemContent` instances (so `Gh3.File` instances and/or `Gh3.Dir` instances)
+- `getFileByName(file_name)` : return a `Gh3.File` instance found by name (you have to call `fetchContents()` before)
+- `getDirByName(dir_name)` : return a `Gh3.Dir` instance found by name (you have to call `fetchContents()` before)
+- `eachContent(callback)` : execute `callback` for each content item of the directory (you have to call `fetchContents()` before)
+
+###Get contents of a directory
+
+	master.fetchContents(function () {
+
+		var dir = master.getDirByName('_posts');
+
+		dir.fetchContents(function () {
+			console.log(dir.getContents());
+			
+			dir.eachContent(function (content) {
+				console.log(content.name, content.type, content.size);
+
+			});
+		});
+		
+	});
+
+See [/samples/06-dir.html](/samples/06-dir.html)
+
+##GitHub Gists : Gh3.Gists
+
+####Methods of Gh3.Gists
+
+- `fetch(callback, callbackErr)` : retrieve `Gh3.Gist` instances of a user
+- `getGists()` : return array of `Gh3.Gist` instances (you have to call `fetch()` before)
+- `eachGist` : execute `callback` for each gist of the user (you have to call `fetch()` before)
+
+###Get public gists of a user
+
+	var GistsOfK33g = new Gh3.Gists(new Gh3.User("k33g"));
+	
+	GistsOfK33g.fetch(function () {
+		GistsOfK33g.eachGist(function (gist) {
+			console.log(gist.description, gist.id);
+		});
+	});
+
+See [/samples/07-gists.html](/samples/07-gists.html)
+
+##GitHub Gist : Gh3.Gist
+
+####Methods of Gh3.Gist
+
+- `fetchContents(callback, callbackErr)` : retrieve contents (files) of the gist
+- `fetchComments(callback, callbackErr)` : retrieve comments of the gist
+- `getFileByName()` : return a file of the gist, found by his name
+- `getFiles()` : return a list of files objects
+- `eachFile(callback)` : execute `callback` for each file of the gist (you have to call `fetchContents()` before)
+- `getComments()` : return an array of `Gh3.GistComment` instances
+- `eachComment(callback)` : execute `callback` for each comment of the gist (you have to call `fetchComments()` before)
+
+###Get a public gist by id
+
+	var oneGist = new Gh3.Gist({id:"2287018"});
+	
+	oneGist.fetchContents(function(){
+		console.log("oneGist : ", oneGist);
+		console.log("Files : ", oneGist.files);
+		
+		oneGist.eachFile(function (file) {
+			console.log(file.filename, file.language, file.type, file.size);
+		});
+	});
+
+####Get content of a file of a gist
+
+	console.log(oneGist.getFileByName("use.thing.js").content);
+
+See [/samples/07-gists.html](/samples/07-gists.html)
+
+###Get comments of a public gist
+
+	var anOtherGist = new Gh3.Gist({id:"1096826"});
+
+	anOtherGist.fetchContents(function(){
+		
+		anOtherGist.fetchComments(function () {
+			
+			anOtherGist.eachComment(function (comment) {
+				console.log(comment.body, "By ", comment.user.login);
+			});
+		});
+	});
+
+See [/samples/07-gists.html](/samples/07-gists.html)
+
+... to be continued
---
+++ gh3.js
@@ -0,0 +1,499 @@
+/*
+	gh3.js
+	2012.07.25 by k33g
+*/
+
+(function(){
+
+	var Gh3 = this.Gh3 = {}
+	,	Kind
+	,	Base64;
+	
+	Gh3.VERSION = '0.0.1';
+	
+	//Object Model Tools (helpers) like Backbone
+	Kind = function(){};
+	
+	Kind.inherits = function (parent, protoProps, staticProps) {
+		var child
+			, ctor = function(){}
+			, merge = function (destination, source) {
+				for (var prop in source) {
+					destination[prop] = source[prop];
+				}
+		};
+		//constructor ....
+		if (protoProps && protoProps.hasOwnProperty('constructor')) {
+		  child = protoProps.constructor;
+		} else {
+		  child = function(){ parent.apply(this, arguments); };
+		}
+	
+		//inherits from parent
+		merge(child, parent);
+	
+		ctor.prototype = parent.prototype;
+		child.prototype = new ctor();
+	
+		//instance properties
+		if(protoProps) merge(child.prototype, protoProps);
+	
+		//static properties
+		if(staticProps) merge(child, staticProps);
+	
+		// Correctly set child's `prototype.constructor`.
+		child.prototype.constructor = child;
+	
+		// Set a convenience property in case the parent's prototype is needed later.
+		child.__super__ = parent.prototype;
+	
+		return child
+	
+	};
+	Kind.extend = function (protoProps, staticProps) {
+		var child = Kind.inherits(this, protoProps, staticProps);
+		child.extend = this.extend;
+		return child;
+	};
+	
+
+	Base64 = { //http://www.webtoolkit.info/javascript-base64.html
+ 
+		// private property
+		_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+	 	 
+		// public method for decoding
+		decode : function (input) {
+			var output = "";
+			var chr1, chr2, chr3;
+			var enc1, enc2, enc3, enc4;
+			var i = 0;
+	 
+			input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+	 
+			while (i < input.length) {
+	 
+				enc1 = this._keyStr.indexOf(input.charAt(i++));
+				enc2 = this._keyStr.indexOf(input.charAt(i++));
+				enc3 = this._keyStr.indexOf(input.charAt(i++));
+				enc4 = this._keyStr.indexOf(input.charAt(i++));
+	 
+				chr1 = (enc1 << 2) | (enc2 >> 4);
+				chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+				chr3 = ((enc3 & 3) << 6) | enc4;
+	 
+				output = output + String.fromCharCode(chr1);
+	 
+				if (enc3 != 64) {
+					output = output + String.fromCharCode(chr2);
+				}
+				if (enc4 != 64) {
+					output = output + String.fromCharCode(chr3);
+				}
+	 
+			}
+	 
+			output = Base64._utf8_decode(output);
+	 
+			return output;
+	 
+		},
+	 
+		// private method for UTF-8 decoding
+		_utf8_decode : function (utftext) {
+			var string = "";
+			var i = 0;
+			var c = c1 = c2 = 0;
+	 
+			while ( i < utftext.length ) {
+	 
+				c = utftext.charCodeAt(i);
+	 
+				if (c < 128) {
+					string += String.fromCharCode(c);
+					i++;
+				}
+				else if((c > 191) && (c < 224)) {
+					c2 = utftext.charCodeAt(i+1);
+					string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+					i += 2;
+				}
+				else {
+					c2 = utftext.charCodeAt(i+1);
+					c3 = utftext.charCodeAt(i+2);
+					string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+					i += 3;
+				}
+	 
+			}
+	 
+			return string;
+		}
+	 
+	}
+
+
+	Gh3.User = Kind.extend({
+	
+		constructor : function (login) {
+			if (login) {
+				this.login = login; 
+			} else { 
+				throw "login !";
+			}
+		},
+		fetch : function (callback, callbackErr) {
+			var that = this;
+			$.ajax({
+				url : "https://api.github.com/users/"+that.login,
+				dataType: 'jsonp',
+				success : function (res) {
+					for(var prop in res.data) {
+						that[prop] = res.data[prop];
+					}
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		}
+		
+		
+	},{});
+	
+	//Gh.Item = Kind.extend({},{});
+	//Gh3.GitHubObject = Kind.extend({},{});
+
+	/*Gists*/
+
+	Gh3.GistComment = Kind.extend({
+		constructor : function (gistCommentData) {
+			for(var prop in gistCommentData) {
+				this[prop] = gistCommentData[prop];
+			}
+		}
+	},{});
+
+	Gh3.Gist = Kind.extend({
+		constructor : function (gistData) {
+			for(var prop in gistData) {
+				this[prop] = gistData[prop];
+			}
+		},
+		fetchContents : function (callback, callbackErr) {
+			var that = this;
+			$.ajax({
+				url : "https://api.github.com/gists/"+that.id,
+				dataType: 'jsonp',
+				success : function(res) {
+					for(var prop in res.data) {
+						that[prop] = res.data[prop];
+					}
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+		fetchComments : function (callback, callbackErr) {
+			var that = this;
+			that.comments = [];
+			$.ajax({
+				url : "https://api.github.com/gists/"+that.id+"/comments",
+				dataType: 'jsonp',
+				success : function(res) {
+					_.each(res.data, function (comment) {
+						that.comments.push(new Gh3.GistComment(comment));
+					});
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});			
+		},
+		getFileByName : function (name) {
+			return this.files[name];
+		},
+		getFiles : function () {
+			return this.files;
+		},
+		eachFile : function (callback) {
+			for(var fileName in this.files) {
+				callback(this.files[fileName]);
+			}
+		},
+		getComments : function () { return this.comments; },
+		eachComment : function (callback) {
+			_.each(this.comments, function (comment) {
+				callback(comment);
+			});
+		}
+
+	},{});
+
+	Gh3.Gists = Kind.extend({//http://developer.github.com/v3/gists/
+		constructor : function (ghUser) {
+			if (ghUser) this.user = ghUser;
+			this.gists = []
+		}, 
+		fetch : function (callback, callbackErr) {
+			var that = this;
+			$.ajax({
+				url : "https://api.github.com/users/"+that.user.login+"/gists",
+				dataType: 'jsonp',
+				success : function(res) {
+					_.each(res.data, function (gist) {
+						that.gists.push(new Gh3.Gist(gist));
+					});
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+		getGists : function () { return this.gists; },
+		eachGist : function (callback) {
+			_.each(this.gists, function (gist) {
+				callback(gist);
+			});
+		}
+
+	},{});
+
+
+
+
+	Gh3.Commit = Kind.extend({
+		constructor : function (commitInfos) {
+			this.author = commitInfos.author;
+			this.author.email = commitInfos.commit.author.email;
+			this.author.name = commitInfos.commit.author.name;
+			this.date =  commitInfos.commit.author.date;
+			this.message = commitInfos.commit.message;
+			this.sha = commitInfos.sha;
+			this.url = commitInfos.url;
+		}
+	},{});
+
+	Gh3.ItemContent = Kind.extend({
+		constructor : function (contentItem, ghUser, repositoryName, branchName) {
+			for(var prop in contentItem) {
+				this[prop] = contentItem[prop];
+			}
+			if (ghUser) this.user = ghUser;
+			if (repositoryName) this.repositoryName = repositoryName;
+			if (branchName) this.branchName = branchName;
+		}
+
+	},{});
+
+	Gh3.File = Gh3.ItemContent.extend({
+		constructor : function (contentItem, ghUser, repositoryName, branchName) {
+			Gh3.File.__super__.constructor.call(this, contentItem, ghUser, repositoryName, branchName);
+		},
+		fetchContent : function (callback, callbackErr) {
+			var that = this;
+
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.repositoryName+"/contents/"+that.path,
+				dataType: 'jsonp',
+				success : function(res) {
+					that.content = res.data.content;
+					that.rawContent = Base64.decode(res.data.content);
+
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			})
+		},
+		fetchCommits : function (callback, callbackErr) {//http://developer.github.com/v3/repos/commits/
+			var that = this;
+			that.commits = [];
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.repositoryName+"/commits",
+				dataType: 'jsonp',
+				data : {path: that.path },
+				success : function(res) {
+					_.each(res.data, function (commit) {
+						that.commits.push(new Gh3.Commit(commit));
+					});
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			})
+		},
+		getRawContent : function() { return this.rawContent; },
+		getCommits : function () { return this.commits; },
+		getLastCommit : function () {
+			return this.commits[0];
+		},
+		getFirstCommit : function () {
+			return this.commits[this.commits.length-1];
+		},
+		eachCommit : function (callback) {
+			_.each(this.commits, function (branch) {
+				callback(branch);
+			});
+		}
+	},{});
+
+
+	Gh3.Dir = Gh3.ItemContent.extend({
+		constructor : function (contentItem, ghUser, repositoryName, branchName) {
+			Gh3.Dir.__super__.constructor.call(this, contentItem, ghUser, repositoryName, branchName);
+		},
+		fetchContents : function (callback, callbackErr) {
+			var that = this;
+			that.contents = [];
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.repositoryName+"/contents/"+that.path,
+				data : {ref: that.branchName },
+				dataType: 'jsonp',
+				success : function(res) {
+					_.each(res.data, function (item) {
+						if (item.type == "file") that.contents.push(new Gh3.File(item, that.user, that.repositoryName, that.name));
+						if (item.type == "dir") that.contents.push(new Gh3.Dir(item, that.user, that.repositoryName, that.name));
+					});
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+
+		getContents : function() { return this.contents; },
+		getFileByName : function (name) {
+			return _.find(this.contents, function (item) {
+				return item.name == name && item.type == "file";
+			});
+		},
+		getDirByName : function (name) {
+			return _.find(this.contents, function (item) {
+				return item.name == name && item.type == "dir";
+			});
+		},
+		eachContent : function (callback) {
+			_.each(this.contents, function (branch) {
+				callback(branch);
+			});
+		}
+
+	},{});
+
+	Gh3.Branch = Kind.extend({
+		constructor : function (name, sha, url, ghUser, repositoryName) {
+			if (name) this.name = name;
+			if (sha) this.sha = sha;
+			if (url) this.url = url;
+
+			if (ghUser) this.user = ghUser;
+			if (repositoryName) this.repositoryName = repositoryName;
+
+		},
+
+		fetchContents : function (callback, callbackErr) {
+			var that = this;
+			that.contents = [];
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.repositoryName+"/contents/",
+				data : {ref: that.name },
+				dataType: 'jsonp',
+				success : function(res) {
+					_.each(res.data, function (item) {
+						if (item.type == "file") that.contents.push(new Gh3.File(item, that.user, that.repositoryName, that.name));
+						if (item.type == "dir") that.contents.push(new Gh3.Dir(item, that.user, that.repositoryName, that.name));
+					});
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+
+		getContents : function() { return this.contents; },
+		getFileByName : function (name) {
+			return _.find(this.contents, function (item) {
+				return item.name == name && item.type == "file";
+			});
+		},
+		getDirByName : function (name) {
+			return _.find(this.contents, function (item) {
+				return item.name == name && item.type == "dir";
+			});
+		},
+		eachContent : function (callback) {
+			_.each(this.contents, function (branch) {
+				callback(branch);
+			});
+		}
+		
+	},{});
+
+	Gh3.Repository = Kind.extend({
+		constructor : function (name, ghUser) {
+			if (name) this.name = name; 
+
+			if (ghUser) this.user = ghUser;
+
+		},
+		fetch : function (callback, callbackErr) {
+			var that = this;
+			//TODO test user.login & name
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.name,
+				dataType: 'jsonp',
+				success : function(res) {
+					for(var prop in res.data) {
+						that[prop] = res.data[prop];
+					}
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+		fetchBranches : function (callback, callbackErr) {
+			var that = this;
+			that.branches = [];
+			$.ajax({
+				url : "https://api.github.com/repos/"+that.user.login+"/"+that.name+"/branches",
+				dataType: 'jsonp',
+				success : function(res) {
+					_.each(res.data, function (branch) {
+						that.branches.push(new Gh3.Branch(branch.name, branch.commit.sha, branch.commit.url, that.user, that.name));
+					});
+					
+					if (callback) callback(that);
+				},
+				error : function (res) {
+					if (callbackErr) callbackErr(res);
+				}
+			});
+		},
+		getBranches : function () { return this.branches; },
+		getBranchByName : function (name) {
+			return _.find(this.branches, function (branch) {
+				return branch.name == name;
+			});
+		},
+		eachBranch : function (callback) {
+			_.each(this.branches, function (branch) {
+				callback(branch);
+			});
+		}
+
+	},{});
+	
+	
+}).call(this);
